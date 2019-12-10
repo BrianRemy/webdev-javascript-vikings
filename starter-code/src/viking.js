bd814e95a1eb038c3ce6ba111c59fb006f143798
Brian Remy <22299@columbushs.com>
2019-12-10 14:45:01 -0500

    class ended 2 tuesday boogaloo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -18,18 +18,53 @@ class Viking extends Soldier {
         super(health, strength),
         this.name = name
     }
-    recieveDamage(damage){
+    receiveDamage(damage){
+        this.health -= damage
         if(this.health > 0){
-            return (`${this.name} has received ${damage} points of damage`)
-        }
+            return `${this.name} has received ${damage} points of damage`;
+        } 
         else if(this.health <= 0){
-            return (`${this.name} has died in act of combat`)
+            return `${this.name} has died in act of combat`;
         }
+    }
+    battleCry(){
+        return "Odin Owns You All!"
     }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength)
+    }
+    receiveDamage(damage){
+        this.health -= damage
+        if(this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else{
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        let damagedSaxon = new Saxon (health)
+        if(damagedSaxon.health <= 0){
+            this.saxonArmy.splice
+        }
+        return damagedSaxon.receiveDamage
+    }
+}
